b0c1ca0fc609aecfb06436a83b6c0a983edad6ef
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2024-11-01 16:05:33 +0530

    updated scripts

--- runUnittest.sh
+++ runUnittest.sh
@@ -8,7 +8,7 @@ failures=$(xmllint --xpath 'string(//testsuite/@failures)' report.xml)
 # Assuming "passed" means tests - (errors + failures)
 passed=$((tests - errors - failures))
 githubComment="Test Results for commit ${commitSHA}: \n- Total Tests: ${tests}\n- Passed: ${passed}\n- Failed: ${failures}"
-curl -H "Authorization: token ${env.GITHUB_TOKEN}" \
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
         -X POST \
         -d '{ "body": "${githubComment}" }' \
-        "https://api.github.com/repos/${env.GITHUB_REPO}/commits/${commitSHA}/comments"
+        "https://api.github.com/repos/${GITHUB_REPO}/commits/${commitSHA}/comments"
